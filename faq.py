29e5cbe5fc8680c05780ec075545736cb7e1a4fc
Shreyas Kapur <shreyask@mit.edu>
2017-07-20 09:52:23 +0530

    Update FAQ

--- faq.py
+++ faq.py
@@ -7,7 +7,7 @@ COMMANDS = {
 
 	Hi! <br><br>
 
-	You need to be an enrolled undergraduate as of September 16th to participate in HackMIT. We can whitelist your personal email if you send us some proof of admission and enrollment. This is usually a picture of your student ID. <br><br>
+	You need to be an enrolled undergraduate as of September 16th to participate in HackMIT. We can whitelist your email if you send us some proof of admission and enrollment. This is usually a picture of your student ID. <br><br>
 
 	If your college provides you with an educational email, we can whitelist the entire domain so that you and your classmates can register for HackMIT. <br><br>
 
